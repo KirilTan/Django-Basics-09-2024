c453ef474b368f63b7570905ff8c1df210482de9
kiril <kiriltanev@proton.me>
2024-10-09 21:48:49 +0300

    more complex views created

--- djangoIntroduction/djangoProject/djangoProject/todo_app/views.py
+++ djangoIntroduction/djangoProject/djangoProject/todo_app/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from djangoProject.todo_app.models import Task
+
+
 # Create your views here.
 def main_page(request):
-    return HttpResponse("<h1>Hello, this is the main page!</h1>")
+    tasks = Task.objects.all()
+    result = '\n'.join([
+        '<h1>Hello, this is the main page!</h1>',
+        '<ul>',
+        *[f'<li>{task.name} - {task.description} - created at {task.created_at}</li>' for task in tasks],
+        '</ul>',
+    ])
+
+    print(result)
+
+    return HttpResponse(result)
 
 def store(request):
     return HttpResponse("<h1>Hello, this is the store page!</h1>")
--- djangoIntroduction/djangoProject/djangoProject/urls.py
+++ djangoIntroduction/djangoProject/djangoProject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/main-page/', include('djangoProject.todo_app.urls')),
+    path('', include('djangoProject.todo_app.urls')),
 ]
