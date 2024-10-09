d60b14f4ac1dc6ff770ce6e4bdeeb7ea2707a8f9
kiril <kiriltanev@proton.me>
2024-10-09 20:39:42 +0300

    migrated db

--- djangoIntroduction/djangoProject/djangoProject/settings.py
+++ djangoIntroduction/djangoProject/djangoProject/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-                     'djangoProject.todo_app.apps.TodoListConfig'
 ] + MY_APPS
 
 MIDDLEWARE = [
@@ -79,12 +78,15 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todo_db",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
