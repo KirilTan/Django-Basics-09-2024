d67e252a4dbae21f4435e3045937bb44be23f8cd
kiril <kiriltanev@proton.me>
2024-10-09 22:08:36 +0300

    admin customization added

--- djangoIntroduction/djangoProject/djangoProject/todo_app/admin.py
+++ djangoIntroduction/djangoProject/djangoProject/todo_app/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
+from djangoProject.todo_app.models import Task
+
+
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name',
+    )
+
+    list_filter = (
+        'created_at',
+        'name',
+    )
+
+    list_display = (
+        'id',
+        'name',
+        'description',
+        'created_at'
+    )
+
+    fieldsets = \
+        (
+            (
+                'Name and Description',
+                {
+                    'fields':
+                        (
+                            'name',
+                            'description',
+                        )
+                }
+            ),
+        )
