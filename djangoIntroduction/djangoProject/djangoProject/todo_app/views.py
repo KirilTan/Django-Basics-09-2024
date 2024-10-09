2a54517bb31efabf5626d9d2d16d010769e219e2
kiril <kiriltanev@proton.me>
2024-10-09 22:28:36 +0300

    template created with DTL

--- djangoIntroduction/djangoProject/djangoProject/todo_app/views.py
+++ djangoIntroduction/djangoProject/djangoProject/todo_app/views.py
@@ -7,16 +7,11 @@ from djangoProject.todo_app.models import Task
 # Create your views here.
 def main_page(request):
     tasks = Task.objects.all()
-    result = '\n'.join([
-        '<h1>Hello, this is the main page!</h1>',
-        '<ul>',
-        *[f'<li>{task.name} - {task.description} - created at {task.created_at}</li>' for task in tasks],
-        '</ul>',
-    ])
+    context = {
+        'tasks': [],
+    }
 
-    print(result)
-
-    return HttpResponse(result)
+    return render(request, 'tasks/index.html', context)
 
 def store(request):
     return HttpResponse("<h1>Hello, this is the store page!</h1>")
