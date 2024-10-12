857648f62d42a4b8bbdc5f0e1d02e051eab2a7a0
kiril <kiriltanev@proton.me>
2024-10-12 17:13:04 +0300

    app urls updated

--- djangoIntroduction/djangoProject1/djangoProject1/departments/urls.py
+++ djangoIntroduction/djangoProject1/djangoProject1/departments/urls.py
@@ -3,12 +3,13 @@ from djangoProject1.departments import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:pk>/', views.view_with_int_pk),
     path('<str:name>/', views.view_with_name),
     path('id/', include([
         path('<int:pk>/<slug:slug>/', views.view_with_pk_and_slug),
     ])),
-    path('<slug:slug>/', views.view_with_slug),
-    path('<int:pk>/', views.view_with_int_pk),
+    path('dp/', include([
+        path('<slug:slug>/', views.view_with_slug),
+    ])),
     path('<param>/', views.view_with_args_and_kwargs),
-
 ]
