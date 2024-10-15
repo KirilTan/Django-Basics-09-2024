4c66d5d592d94ab3d54c9b4f56d995731f1a2b3f
kiril <kiriltanev@proton.me>
2024-10-15 23:43:35 +0300

    feat: display tagged animals in photo admin panel

--- workshop/petstagram/petstagram/photos/admin.py
+++ workshop/petstagram/petstagram/photos/admin.py
@@ -6,12 +6,18 @@ from petstagram.photos.models import Photo
 # Register your models here.
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
+
     list_display = (
         'id',
-        'description',
         'date_of_publication',
+        'description',
         'location',
+        'get_tagged_pets',
     )
+
+    @staticmethod
+    def get_tagged_pets(obj):
+        return ', '.join(str(pet) for pet in obj.tagged_pets.all())
 
     list_filter = (
         'date_of_publication',
