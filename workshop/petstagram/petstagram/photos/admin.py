5476d0875f50791bc0c4c442c60a6e7c8910ecbd
kiril <kiriltanev@proton.me>
2024-10-15 23:37:10 +0300

    feat: photo admin

--- workshop/petstagram/petstagram/photos/admin.py
+++ workshop/petstagram/petstagram/photos/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from petstagram.photos.models import Photo
+
+
 # Register your models here.
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'description',
+        'date_of_publication',
+        'location',
+    )
+
+    list_filter = (
+        'date_of_publication',
+        'location',
+        'description',
+    )
+
+    search_fields = (
+        'description',
+        'location',
+    )
